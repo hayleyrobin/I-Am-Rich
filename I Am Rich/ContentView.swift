47521494c596dde98b8424a4dda1700f89608e47
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2022-01-22 18:55:33 -0500

    Used Z and VStack to add image, background color and text

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                    .font(.system(size: 40))
+                Image("diamond").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
